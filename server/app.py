ad72c52a1ca570317c97927fcea5e83d40730611
Ivan-Nebogatikov <ivan.nebogatikov@gmail.com>
2020-05-01 22:13:03 +0500

    Added table test usage

--- server/app.py
+++ server/app.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask import request
 import json
-import Checker
+import boto3
+#import Checker
 
 app = Flask(__name__)
 
+client = boto3.Session(
+        aws_access_key_id='AKIA6RTUQT2ZXZIL2HMX',
+        aws_secret_access_key='a'
+    )
+
+dynamodb = client.resource('dynamodb', region_name='us-east-2')
+table = dynamodb.Table('CachedCorrections')
 
 @app.route('/correct', methods=['POST'])
 def correct():
+
     z = request.json
-    words = list(Checker.correct_core(z["text"]))
+    words = ['a']#list(Checker.correct_core(z["text"]))
 
     best_match = ''
     for word in words:
@@ -22,4 +31,11 @@ def correct():
 
 
 if __name__ == '__main__':
+
+    table.put_item(
+        Item={
+            'Input': '我不知都',
+            'Corrected': "我不知道"
+        }
+    )
     app.run(host='0.0.0.0', debug=True)
